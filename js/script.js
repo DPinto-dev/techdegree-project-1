2b4db4811a8db7e6398b992ac83162beea66753b
JoePinto <diogosmp@gmail.com>
2019-07-14 01:05:48 -0700

    Completed basic structure of the app

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ project 1 - A Random Quote Generator
 
 // Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
 
-
 /*** 
   Create the array of quote objects and name it `quotes`.
   Add at least five quote objects to the `quotes` array.
@@ -15,17 +14,11 @@ project 1 - A Random Quote Generator
   Use console.log() to log your array of quotes to the console.
 ***/
 
-
-
-
 /***
   Create the `getRandomQuote` function to:
    - Create a variable to store a random number 
    - Cse the random number to `return` a random quote object from the `quotes` array.
 ***/
-
-
-
 
 /***
   Create the `printQuote` function to: 
@@ -40,17 +33,94 @@ project 1 - A Random Quote Generator
    - Set the `innerHTML` of the `quote-box` div to the HTML string. 
 ***/
 
-
-
-
 /***
   When the "Show another quote" button is clicked, the event listener 
   below will be triggered, and it will call, or "invoke", the `printQuote` 
   function. So do not make any changes to the line of code below this 
   comment.
 ***/
+// Remember to delete the comments that came with this file, and replace them with your own code comments.
 
+
+/*** SELECTORS ***/
+// ** DP ** Instead of using the selectors we will just write the entire HTML inside printString and then update the innerHTML of #quote-box
+var quoteSelect = document.getElementsByClassName(".quote");
+var sourceSelect = document.getElementsByClassName(".source");
+var citationSelect = document.getElementsByClassName(".citation");
+var yearSelect = document.getElementsByClassName(".year");
+var categorySelect = document.getElementsByClassName(".category");
+
+//Loads a new quote when user clicks the button
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+var quotes = [
+  {
+    quote: "Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.",
+    source: "Patrick McKenzie",
+    citation: "Twitter",
+    year: 2016,
+    category: "Inspirational"
+  },
+  {
+    quote: "Tell me, and I will forget. Show me, and I will remember. Involve me, and I will understand forever.",
+    source: "Xunzi - Confucian Philosopher",
+    citation: "",
+    year: "",
+    category: "Inspirational",
+  },
+  {
+    quote: "There are only two hard problems in Computer Science: cache invalidation and naming things",
+    source: "Phil Karlton",
+    citation: "",
+    year: "1996",
+    category: "Funny",
+  },
+  {
+    quote: "",
+    source: "",
+    citation: "",
+    year: "",
+    category: "",
+  },
+  {
+    quote: "",
+    source: "",
+    citation: "",
+    year: "",
+    category: "",
+  },
+];
 
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+//Generates a random int from 0 to the size of the quotes array and returns the quote at that corresponding index
+function getRandomQuote() {
+  var ranQuote = Math.floor(Math.random() * quotes.length);
+  return quotes[ranQuote];
+  }
+  
+  function printQuote() {
+    var chosenQuote = getRandomQuote();
+    var printString = "";
+    printString = '<p class="quote">' + chosenQuote.quote + '</p><p class="source">' + chosenQuote.source;
+
+    if (chosenQuote.citation !== ""){
+      printString += '<span class="citation">' + chosenQuote.citation + '</span>';
+    }
+    if (chosenQuote.year !== ""){
+      printString += '<span class="year">' + chosenQuote.year + '</span>';      
+    }
+    if (chosenQuote.category !== ""){
+      printString += '<span class="category">' + chosenQuote.category + '</span>';
+    }
+
+    printString += '</p>';
+    document.getElementById("quote-box").innerHTML = printString;
+  }
+
+    // create a variable that calls the getRandomQuote() function
+    // create a variable that initiates your HTML string
+    // using the template in the project instructions, add the two default quote properties, quote and source
+    // if there is a quote.citation property, add it the string
+    // if there is a quote.year property, add it the string
+    // close the string with the necessary closing HTML tags
+    // set the innnerHTML of the .quote-box to the complete HTML string
+    
---
+++ projectGrading.md
@@ -0,0 +1,26 @@
+Create Array of Objects
+    * Array of objects is named quotes.
+    * Array contains at least 5 quote objects.
+    * Array is free of errors.
+
+Quote Objects
+    * Objects in array include quote, source, citation and year properties.
+    * Objects in array include additional properties, such as categorization tags
+    * Additional properties are displayed on screen when present in quote object.
+
+`getRandomQuote` function
+    * Function is named getRandomQuote and returns a random object from the quotes array.
+
+`printQuote` function
+    * Function is named printQuote
+    * It calls the getRandomQuote function.
+    * It prints the quote to the page using the example HTML string format.
+    * Quotes change automatically after a certain amount of time passes.
+    * Background color changes each time the quote changes.
+
+Code comments
+    * Comments are included in the code.
+
+
+** Our Additional Ideas: **
+     
